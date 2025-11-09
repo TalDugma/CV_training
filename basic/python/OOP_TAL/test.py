9eb6db38698bdefdca155f18de2457310d39bdf4
Tal Dugma <taldugma@gmail.com>
2025-11-09 14:57:23 +0200

    change hirearchies of folders

--- basic/python/OOP_TAL/test.py
+++
@@ -1,3 +0,0 @@
-import shapes
-
-line = Line()
--- basic/python/OOP_TAL/test_rotation.py
+++
@@ -1,6 +0,0 @@
-import shapes
-from Board import Board
-board = Board()
-shapes = shapes.ShapeFactory.create_shapes_from_json("composite_shape.json")
-
-
